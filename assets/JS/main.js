fe397338e2603fe8b2733457e4067002cd54b794
Mateus <mateuslopes1717@gmail.com>
2022-10-10 20:24:37 -0300

    add soft skills

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -7,11 +7,33 @@ function updateProfileInfo(profileData) {
     
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
+
+    const graduation = document.getElementById('profile.graduation')
+    graduation.innerText = profileData.graduation
+    
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel: ${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 async function GetProfileData(){
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
-    
+    updateSoftSkills(profileData)
 }
 
 GetProfileData()
